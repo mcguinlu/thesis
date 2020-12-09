2cf1059c8b0a9a636e76c59f3874810f34799c7f
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2020-12-09 16:55:23 +0000

    Double-saving

--- R/Chp2-Systematic-Review.R
+++ R/Chp2-Systematic-Review.R
@@ -1,26 +1,26 @@
-# ---- prisma-flow
+# ---- prismaflow
 
-png(file = "figures/sys-rev/prismaflow.png", bg = "transparent")
-PRISMAstatement::prisma(
-  found = 16109,
-  found_other = 0,
-  no_dupes = 16109,
-  screened = 16109,
-  screen_exclusions = 15722,
-  full_text = 387,
-  full_text_exclusions = 0,
-  qualitative = 0,
-  quantitative = 0,
-  labels = NULL,
-  extra_dupes_box = FALSE,
-  dpi = 72,
-  font_size = 10)
-dev.off()
+# png(file = "figures/sys-rev/prismaflow.png", bg = "transparent")
+# PRISMAstatement::prisma(
+#   found = 16109,
+#   found_other = 0,
+#   no_dupes = 16109,
+#   screened = 16109,
+#   screen_exclusions = 15722,
+#   full_text = 387,
+#   full_text_exclusions = 0,
+#   qualitative = 0,
+#   quantitative = 0,
+#   labels = NULL,
+#   extra_dupes_box = FALSE,
+#   dpi = 72,
+#   font_size = 10)
+# dev.off()
+# 
+# knitr::include_graphics("figures/sys-rev/prismaflow.png", 
+#                         )
 
-knitr::include_graphics("figures/sys-rev/prismaflow.png", 
-                        )
-
-# ---- gwet-table
+# ---- gwettable
 
 kappa.table <- data.frame(
   stringsAsFactors = FALSE,
@@ -42,13 +42,9 @@ kappa.table <- data.frame(
   )
 )
 
-mal::aca_simple_table(kappa.table,
-                      autofit = FALSE,
-                      fontsize = 10) %>%
-  flextable::width(j = 1:2, 1.5) %>%
-  flextable::set_caption("Test")   
+knitr::kable(kappa.table, booktabs = TRUE)
 
-# ---- agreement-table-inter
+# ---- agreementtableinter
 
 agreement.table.1 <- data.frame(group = rep("Second reviewer decision",3),
                                 reviewer = c("Exclude", "Include", "Total"), 
@@ -56,22 +52,19 @@ agreement.table.1 <- data.frame(group = rep("Second reviewer decision",3),
                                 Include = c(9,22,31), 
                                 Total = c(1253, 48,1301))
 
-
-# mal::aca_simple_table(agreement.table.1)
-
-knitr::kable(agreement.table.1, col.names = c("","","Exclude", "Include","Total"),  caption = 'Inter-rater reliability') %>%
-  kable_styling("striped") %>%
-  add_header_above(c(" " = 2, "Initial screening descision" = 3)) %>%
-  column_spec(1, bold = T) %>%
-  column_spec(2, bold = T, border_right = T) %>%
-  column_spec(4, border_right = T) %>%
-  collapse_rows(columns = 1, valign ="middle") %>%
-  row_spec(2,extra_css = "border-bottom: 1px solid")
+knitr::kable(agreement.table.1, booktabs = TRUE, col.names = c("","","Exclude", "Include","Total"),  caption = 'Inter-rater reliability') %>%
+  kableExtra::kable_styling("striped") %>%
+  kableExtra::add_header_above(c(" " = 2, "Initial screening descision" = 3)) %>%
+  kableExtra::column_spec(1, bold = T) %>%
+  kableExtra::column_spec(2, bold = T) %>%
+  kableExtra::column_spec(4, border_right = T) %>%
+  kableExtra::collapse_rows(columns = 1, valign ="middle") %>%
+  kableExtra::row_spec(2,extra_css = "border-bottom: 1px solid")
 
 
 
 
-# ---- agreement-table-intra
+# ---- agreementtableintra
 
 agreement.table.2 <- data.frame(group = rep("Same reviewer decision",3),
                                 reviewer = c("Exclude", "Include", "Total"), 
@@ -79,7 +72,7 @@ agreement.table.2 <- data.frame(group = rep("Same reviewer decision",3),
                                 Include = c(14,17,31), 
                                 Total = c(1280,21,1301))
 
-knitr::kable(agreement.table.2, col.names = c("","","Exclude", "Include","Total"),  caption = 'Intra-rater reliability') %>%
+knitr::kable(agreement.table.2, booktabs = TRUE, col.names = c("","","Exclude", "Include","Total"),  caption = 'Intra-rater reliability') %>%
   kable_styling("striped") %>%
   add_header_above(c(" " = 2, "Initial screening decision" = 3)) %>%
   column_spec(1, bold = T) %>%
